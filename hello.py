804a9e3b60b69548bb2f4a7be53479104ab50bdd
CrimsonCoder42 <nostro37@gmail.com>
2023-08-07 05:36:50 -0400

    init commit

---
+++ hello.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+print(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/bye")
+def say_bye():
+    return "<p>Bye</p>"
+
+
+if __name__ == "__main__":
+    app.run()
+
+print(f"result{hello_world}" )
